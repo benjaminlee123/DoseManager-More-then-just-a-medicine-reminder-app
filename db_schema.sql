d3da87ea1dc3d63288daee3728e7f1379d279e72
benjaminlee123 <42131842+benjaminlee123@users.noreply.github.com>
2023-07-28 19:06:47 +0800

    Add database schema script

---
+++ db_schema.sql
@@ -0,0 +1,29 @@
+-- db_schema.sql
+
+-- Profile Table
+CREATE TABLE IF NOT EXISTS ProfileTable (
+  Profile_id INTEGER PRIMARY KEY, 
+  Name TEXT, 
+  DateOfBirth DATE
+);
+
+-- Medication table
+CREATE TABLE IF NOT EXISTS MedicationTable (
+  Medication_id INTEGER PRIMARY KEY,
+  Profile_id INTEGER, 
+  MedicationName TEXT,
+  Description TEXT,
+  Frequency TEXT,
+  Dosage TEXT,
+  FOREIGN KEY (Profile_id) REFERENCES ProfileTable (Profile_id)
+);
+
+-- Appointment table
+CREATE TABLE IF NOT EXISTS AppointmentTable (
+  Appointment_id INTEGER PRIMARY KEY,
+  Profile_id INTEGER,
+  AppointmentDate DATE,
+  DoctorName TEXT,
+  AppointmentLocation TEXT,
+  FOREIGN KEY (Profile_id) REFERENCES ProfileTable (Profile_id)
+);
