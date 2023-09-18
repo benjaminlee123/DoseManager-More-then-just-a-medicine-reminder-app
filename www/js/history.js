a133455611ffaf0d3b51a60c302a9eae2b247ff7
benjaminlee123 <42131842+benjaminlee123@users.noreply.github.com>
2023-09-18 18:35:53 +0800

    Final commit

--- www/js/history.js
+++ www/js/history.js
@@ -1,94 +1,103 @@
-
 document.addEventListener("deviceready", displayMissedNotifications);
 
-
 function getProfileIdFromURL() {
-    const urlParams = new URLSearchParams(window.location.search);
-    return {
-        id: urlParams.get("id"),
-        pic: urlParams.get("pic")
-    };
+  const urlParams = new URLSearchParams(window.location.search);
+  return {
+    id: urlParams.get("id"),
+    pic: urlParams.get("pic"),
+  };
 }
 
-
 function handleHomeButtonClick(profile) {
-    const homeBtn = document.getElementById("backToHomeBtn");
-    if (homeBtn) {
-        homeBtn.addEventListener("click", function () {
-            window.location.href = `home.html?id=${profile.id}&pic=${profile.pic}`;
-        });
-    }
+  const homeBtn = document.getElementById("backToHomeBtn");
+  if (homeBtn) {
+    homeBtn.addEventListener("click", function () {
+      window.location.href = `home.html?id=${profile.id}&pic=${profile.pic}`;
+    });
+  }
 }
 
 function displayMissedAppointments(appointments, appointmentList) {
-    for (let appointmentId in appointments) {
-        let appointment = appointments[appointmentId];
-        let formattedDate = new Date(appointment.apptDateTime).toLocaleDateString('en-GB', { day: '2-digit', month: '2-digit', year: 'numeric' });
-        let formattedTime = new Date(appointment.apptDateTime).toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit', hour12: true });
-        appointmentList.innerHTML += `
-            <div class="card mt-4">
+  for (let appointmentId in appointments) {
+    let appointment = appointments[appointmentId];
+    let formattedDate = new Date(appointment.apptDateTime).toLocaleDateString(
+      "en-GB",
+      { day: "2-digit", month: "2-digit", year: "numeric" }
+    );
+    let formattedTime = new Date(appointment.apptDateTime).toLocaleTimeString(
+      "en-US",
+      { hour: "2-digit", minute: "2-digit", hour12: true }
+    );
+    appointmentList.innerHTML += `
+            <div class="card missed-appointment mt-4">
                 <div class="card-body">
                 <h5 class="card-title">Missed appointment at ${appointment.apptLocation} on ${formattedDate}, ${formattedTime}.</h5>
                 </div>
             </div>
         `;
-    }
+  }
 }
 
 function displayMissedMedicines(medicines, medicineList) {
-    for (let medicineId in medicines) {
-        let medicine = medicines[medicineId];
-         
-        let timestampInMillis = medicine.timestamp.seconds * 1000;
-        let formattedDate = new Date(timestampInMillis).toLocaleDateString('en-GB', { day: '2-digit', month: '2-digit', year: 'numeric' });
-        let formattedTime = new Date(timestampInMillis).toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit', hour12: true });
-        
-        medicineList.innerHTML += `
-            <div class="card mt-4">
+  for (let medicineId in medicines) {
+    let medicine = medicines[medicineId];
+
+    let timestampInMillis = medicine.timestamp.seconds * 1000;
+    let formattedDate = new Date(timestampInMillis).toLocaleDateString(
+      "en-GB",
+      { day: "2-digit", month: "2-digit", year: "numeric" }
+    );
+    let formattedTime = new Date(timestampInMillis).toLocaleTimeString(
+      "en-US",
+      { hour: "2-digit", minute: "2-digit", hour12: true }
+    );
+
+    medicineList.innerHTML += `
+            <div class="card missed-medicine mt-4">
                 <div class="card-body">
                 <h5 class="card-title">Missed ${medicine.name} dosage on ${formattedDate}, ${formattedTime}.</h5>
                 </div>
             </div>
         `;
-    }
+  }
 }
 
 function displayMissedNotifications() {
-    const profile = getProfileIdFromURL();
-    const appointmentList = document.getElementById("appointment-list");
-    const medicineList = document.getElementById("medicine-list");
+  const profile = getProfileIdFromURL();
+  const appointmentList = document.getElementById("appointment-list");
+  const medicineList = document.getElementById("medicine-list");
 
-    handleHomeButtonClick(profile);
+  handleHomeButtonClick(profile);
 
-    firebase.firestore()
+  firebase
+    .firestore()
+    .collection("Profiles")
+    .doc(profile.id)
+    .collection("MissedNotifications")
+    .doc("Appointments")
+    .get()
+    .then((doc) => {
+      if (doc.exists) {
+        displayMissedAppointments(doc.data(), appointmentList);
+      }
+      return firebase
+        .firestore()
         .collection("Profiles")
         .doc(profile.id)
         .collection("MissedNotifications")
-        .doc("Appointments")
-        .get()
-        .then(doc => {
-            if (doc.exists) {
-                displayMissedAppointments(doc.data(), appointmentList);
-            }
-            return firebase.firestore()
-                .collection("Profiles")
-                .doc(profile.id)
-                .collection("MissedNotifications")
-                .doc("Medicine")
-                .get();
-        })
-        .then(doc => {
-            if (doc.exists) {
-                displayMissedMedicines(doc.data(), medicineList);
-            }
-        })
-        .catch(error => {
-            console.error("Error getting missed notifications: ", error);
-        });
-        var mainProfileId = profile.id;
-  var mainProfileRef = firestore
-    .collection("Profiles")
-    .doc(mainProfileId);
+        .doc("Medicine")
+        .get();
+    })
+    .then((doc) => {
+      if (doc.exists) {
+        displayMissedMedicines(doc.data(), medicineList);
+      }
+    })
+    .catch((error) => {
+      console.error("Error getting missed notifications: ", error);
+    });
+  var mainProfileId = profile.id;
+  var mainProfileRef = firestore.collection("Profiles").doc(mainProfileId);
 
   mainProfileRef
     .get()
@@ -98,7 +107,7 @@ function displayMissedNotifications() {
         var userName = userData.name;
 
         var userProfileName = document.getElementById("userProfileName");
-        userProfileName.textContent = userName+ "'s Missed Notification Log";
+        userProfileName.textContent = userName + "'s Missed Notification Log";
       } else {
         console.log("User document not found");
       }
@@ -107,5 +116,3 @@ function displayMissedNotifications() {
       console.error("Error getting user document: ", error);
     });
 }
-
-
--- www/js/upcomingAppts.js
+++ www/js/upcomingAppts.js
@@ -1,45 +1,44 @@
 document.addEventListener("deviceready", displayData);
 
 function displayData() {
-    var apptList = document.getElementById("appt-list");
-    var upcomingAppts = 0;
-    var missedAppts = 0;
-    apptList.innerHTML = "";
+  var apptList = document.getElementById("appt-list");
+  var upcomingAppts = 0;
+  var missedAppts = 0;
+  apptList.innerHTML = "";
 
-    var profile = getProfileIdFromURL();
-    var mainProfileRef = firestore.collection("Profiles").doc(profile.id);
-    var subCollectionRef = mainProfileRef.collection("Appointments");
+  var profile = getProfileIdFromURL();
+  var mainProfileRef = firestore.collection("Profiles").doc(profile.id);
+  var subCollectionRef = mainProfileRef.collection("Appointments");
 
-    subCollectionRef
-        .orderBy("apptDateTime")
-        .get()
-        .then(function(querySnapshot) {
-            querySnapshot.forEach(function(subDoc) {
-                var status = determineAppointmentStatus(subDoc, subCollectionRef);
-                if (status === "upcoming") upcomingAppts++;
-                else missedAppts++;
+  subCollectionRef
+    .orderBy("apptDateTime")
+    .get()
+    .then(function (querySnapshot) {
+      querySnapshot.forEach(function (subDoc) {
+        var status = determineAppointmentStatus(subDoc, subCollectionRef);
+        if (status === "upcoming") upcomingAppts++;
+        else missedAppts++;
 
-                var apptCard = renderAppointmentCard(subDoc, status);
-                apptList.innerHTML += apptCard;
-            });
+        var apptCard = renderAppointmentCard(subDoc, status);
+        apptList.innerHTML += apptCard;
+      });
 
-            updateAppointmentCounts(upcomingAppts, missedAppts);
-            attachEventListeners(subCollectionRef);
+      updateAppointmentCounts(upcomingAppts, missedAppts);
+      attachEventListeners(subCollectionRef);
+    })
+    .catch(function (error) {
+      console.error("Error getting appointment documents: ", error);
+    });
 
-        })
-        .catch(function(error) {
-            console.error("Error getting appointment documents: ", error);
-        });
-
-    setupNavigationButtons(profile);
+  setupNavigationButtons(profile);
 }
 
 function getProfileIdFromURL() {
-    var urlParams = new URLSearchParams(window.location.search);
-    return {
-        id: urlParams.get("id"),
-        pic: urlParams.get("pic")
-    };
+  var urlParams = new URLSearchParams(window.location.search);
+  return {
+    id: urlParams.get("id"),
+    pic: urlParams.get("pic"),
+  };
 }
 
 function determineAppointmentStatus(subDoc, subCollectionRef) {
@@ -48,21 +47,19 @@ function determineAppointmentStatus(subDoc, subCollectionRef) {
   var currentDateTime = new Date();
 
   if (subDocData.status === "upcoming" && apptDateTime < currentDateTime) {
-      subCollectionRef.doc(subDoc.id).update({ status: "missed" });
-      return "missed";
+    subCollectionRef.doc(subDoc.id).update({ status: "missed" });
+    return "missed";
   }
-  
+
   return subDocData.status;
 }
 
-
-
 function renderAppointmentCard(subDoc, status) {
-    var subDocData = subDoc.data();
-    var formattedDate = formatDate(new Date(subDocData.apptDateTime));
+  var subDocData = subDoc.data();
+  var formattedDate = formatDate(new Date(subDocData.apptDateTime));
 
-    if (status === "upcoming") {
-        return `
+  if (status === "upcoming") {
+    return `
             <div id="upcomingAppt" class="card mt-4 rounded-5">
                 <div class="card-body">
                     <h5 class="card-title">${subDocData.apptLocation}</h5>
@@ -70,13 +67,14 @@ function renderAppointmentCard(subDoc, status) {
                     <p id="docName" class="card-text fw-bold">${subDocData.docName}</p>
                     <p id="apptStatus">Appointment Status:</p>
                     <p id="upcoming">Upcoming</p> 
+                    <div>
+                        <button class="edit-button btn btn-primary" data-item-id="${subDoc.id}">Edit</button>
+                    </div>
                 </div>
-                <div>
-                    <button class="edit-button btn btn-primary" data-item-id="${subDoc.id}">Edit</button>
-                </div>
+                
             </div>`;
-    } else {
-        return `
+  } else {
+    return `
             <div id="missedAppt" class="card mt-4 rounded-5">
                 <div class="card-body">
                     <h5 class="card-title">${subDocData.apptLocation}</h5>
@@ -84,81 +82,92 @@ function renderAppointmentCard(subDoc, status) {
                     <p id="docName" class="card-text fw-bold">${subDocData.docName}</p>
                     <p id="apptStatus">Appointment Status:</p> 
                     <p id="missed">Missed</p> 
+                    <div>
+                        <button class="btn btn-danger deleteButton" data-item-id="${subDoc.id}">Delete</button>
+                    </div>
                 </div>
-                <div>
-                    <button class="btn btn-danger deleteButton" data-item-id="${subDoc.id}">Delete</button>
-                </div>
+                
             </div>`;
-    }
+  }
 }
 
 function attachEventListeners(subCollectionRef) {
   var deleteButtons = document.querySelectorAll(".deleteButton");
-  deleteButtons.forEach(button => {
-      button.addEventListener("click", function(event) {
-          handleDeleteButtonClick(event, subCollectionRef);
-      });
+  deleteButtons.forEach((button) => {
+    button.addEventListener("click", function (event) {
+      handleDeleteButtonClick(event, subCollectionRef);
+    });
   });
 
   var editButtons = document.querySelectorAll(".edit-button");
-  editButtons.forEach(button => button.addEventListener("click", handleEditButtonClick));
+  editButtons.forEach((button) =>
+    button.addEventListener("click", handleEditButtonClick)
+  );
 }
 
 function handleDeleteButtonClick(event, subCollectionRef) {
   var itemId = event.target.getAttribute("data-item-id");
   var profile = getProfileIdFromURL();
   if (itemId && confirm("Are you sure you want to delete this appointment?")) {
-      subCollectionRef.doc(itemId)
-          .delete()
-          .then(function() {
-              console.log("Appointment deleted successfully");
-              window.location.href = `upcomingappt.html?id=${profile.id}&pic=${profile.pic}`;
-          })
-          .catch(function(error) {
-              console.error("Error deleting appointment:", error);
-          });
+    subCollectionRef
+      .doc(itemId)
+      .delete()
+      .then(function () {
+        console.log("Appointment deleted successfully");
+        window.location.href = `upcomingappt.html?id=${profile.id}&pic=${profile.pic}`;
+      })
+      .catch(function (error) {
+        console.error("Error deleting appointment:", error);
+      });
   }
 }
 
-
 function handleEditButtonClick(event) {
-    var itemId = event.target.getAttribute("data-item-id");
-    var profile = getProfileIdFromURL();
-    if (itemId) {
-        window.location.href = `editAppt.html?id=${profile.id}&pic=${profile.pic}&apptId=${itemId}`;
-    }
+  var itemId = event.target.getAttribute("data-item-id");
+  var profile = getProfileIdFromURL();
+  if (itemId) {
+    window.location.href = `editAppt.html?id=${profile.id}&pic=${profile.pic}&apptId=${itemId}`;
+  }
 }
 
 function setupNavigationButtons(profile) {
-    var addApptsButton = document.getElementById("addApptBtn");
-    var medicationFooterButton = document.getElementById("medication-footer");
-    var appointmentFooterButton = document.getElementById("appointment-footer");
-    var profileFooterButton = document.getElementById("profile-footer");
+  var addApptsButton = document.getElementById("addApptBtn");
+  var medicationFooterButton = document.getElementById("medication-footer");
+  var appointmentFooterButton = document.getElementById("appointment-footer");
+  var profileFooterButton = document.getElementById("profile-footer");
 
-    addApptsButton.addEventListener("click", () => navigateTo(`addNewAppt.html?id=${profile.id}&pic=${profile.pic}`));
-    medicationFooterButton.addEventListener("click", () => navigateTo(`home.html?id=${profile.id}&pic=${profile.pic}`));
-    appointmentFooterButton.addEventListener("click", () => navigateTo(`upcomingappt.html?id=${profile.id}&pic=${profile.pic}`));
-    profileFooterButton.addEventListener("click", () => navigateTo(`profile.html?id=${profile.id}&pic=${profile.pic}`));
+  addApptsButton.addEventListener("click", () =>
+    navigateTo(`addNewAppt.html?id=${profile.id}&pic=${profile.pic}`)
+  );
+  medicationFooterButton.addEventListener("click", () =>
+    navigateTo(`home.html?id=${profile.id}&pic=${profile.pic}`)
+  );
+  appointmentFooterButton.addEventListener("click", () =>
+    navigateTo(`upcomingappt.html?id=${profile.id}&pic=${profile.pic}`)
+  );
+  profileFooterButton.addEventListener("click", () =>
+    navigateTo(`profile.html?id=${profile.id}&pic=${profile.pic}`)
+  );
 }
 
 function navigateTo(url) {
-    window.location.href = url;
+  window.location.href = url;
 }
 
 function updateAppointmentCounts(upcoming, missed) {
-    document.getElementById("upcoming-appts").textContent = upcoming;
-    document.getElementById("missed-appts").textContent = missed;
+  document.getElementById("upcoming-appts").textContent = upcoming;
+  document.getElementById("missed-appts").textContent = missed;
 }
 
 function formatDate(inputDate) {
-    const options = {
-        year: "numeric",
-        month: "long",
-        day: "numeric",
-        hour: "2-digit",
-        minute: "2-digit"
-    };
-    return inputDate.toLocaleDateString("en-US", options);
+  const options = {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+    hour: "2-digit",
+    minute: "2-digit",
+  };
+  return inputDate.toLocaleDateString("en-US", options);
 }
 
 function googleTranslateElementInit() {
