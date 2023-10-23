956f27f123582686e90316d2cadcc725b2e5dd93
benjaminlee123 <benjaminleecl@gmail.com>
2023-10-23 15:02:05 +0800

    Update init.js

--- www/js/init.js
+++ www/js/init.js
@@ -1,12 +1,6 @@
 document.addEventListener("deviceready", function() {
     const firebaseConfig = {
-        apiKey: "AIzaSyAt4SUmSwvkHdas68AYQdjOe7fkfL547gQ",
-        authDomain: "dosemanager-d0236.firebaseapp.com",
-        projectId: "dosemanager-d0236",
-        storageBucket: "dosemanager-d0236.appspot.com",
-        messagingSenderId: "373646054095",
-        appId: "1:373646054095:web:89660fa48e041a7d231dba",
-        measurementId: "G-XDL965JQ9H"
+    apiKey: "YOUR_API_KEY_HERE",
     };
 
     if (!firebase.apps.length) {
