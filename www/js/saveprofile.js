7b56b97f6beee1519e91a13a211f7d192ac1a416
benjaminlee123 <benjaminleecl@gmail.com>
2023-10-23 15:03:50 +0800

    Update saveprofile.js

--- www/js/saveprofile.js
+++ www/js/saveprofile.js
@@ -2,15 +2,7 @@ document.addEventListener("deviceready", addProfile);
   
   function addProfile(){
   // Firebase config
-  const firebaseConfig = {
-    apiKey: "AIzaSyAt4SUmSwvkHdas68AYQdjOe7fkfL547gQ",
-    authDomain: "dosemanager-d0236.firebaseapp.com",
-    projectId: "dosemanager-d0236",
-    storageBucket: "dosemanager-d0236.appspot.com",
-    messagingSenderId: "373646054095",
-    appId: "1:373646054095:web:89660fa48e041a7d231dba",
-    measurementId: "G-XDL965JQ9H",
-  };
+  const firebaseConfig =  apiKey: "YOUR_API_KEY_HERE",
 
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
